a25c00cc2957f6b802937af6f86466098a05e6ac
Alisa Khieu <47487416+anqkhieu@users.noreply.github.com>
2019-02-13 18:09:30 -0800

    Update Ball.java

--- brickbreaker/Ball.java
+++ brickbreaker/Ball.java
@@ -1,9 +1,4 @@
-/**
- * Write a description of class Ball here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Ball
 {
     // instance variables - replace the example below with your own  
